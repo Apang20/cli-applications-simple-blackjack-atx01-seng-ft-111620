447722f2ca8d446db5ed5d83543e264ac41e7de8
Avelon Pang <avelonpang@gmail.com>
2020-11-10 21:18:53 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,7 +36,8 @@ def hit?(current_card_total)
   elsif input == "h"
   deal_card + current_card_total
 else
-  puts "Please enter a valid command"
+  #puts "Please enter a valid command"
+  invalid_command
   prompt_user
 end
 end
